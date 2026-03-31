29db9f8936ef40f09c8ebb0dd37d6932e7304293
shivamy009 <shivam0342005@gmail.com>
2026-03-31 22:36:31 +0530

    first commit

---
+++ qlink_chatbot/main.py
@@ -0,0 +1,33 @@
+import os
+
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+from qlink_chatbot.routes.general_routes import general_router
+from qlink_chatbot.routes.ws_routes import (
+    ws_router,
+)
+from qlink_chatbot.utils.logger_config import logger
+
+app = FastAPI(
+    title="Qlink JR backend API",
+    version="0.1.0",
+    redoc_url=None,
+    docs_url=None,
+    openapi_url=None,
+)
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["JR"]
+sessions_collection = db["users"]
+
+app.include_router(general_router, prefix="/api/web")
+app.include_router(ws_router)
+
+@app.get("/ping")
+def ping():
+    logger.info("Ping endpoint called")
+    return {"message": "Qlink <> Jaipur Rugs backend API is up and running"}
+
+logger.info("Qlink JR backend initialized successfully.")
